087aeebc6c349b068a4bd0dff0cd2e319a0b1302
Jaehyung Lee <anddy0622@gmail.com>
2022-09-16 14:48:44 -0400

    executable code

--- src/vocab_main.py
+++ src/vocab_main.py
@@ -656,7 +656,6 @@ class ExecuteCode:
 # en: English
 # zh-cn: Chinese
 users = [(None, "en"), ("Stella", "en"), ("Suru", "ko"), ("Mike", "ko"), ("Taylor", "US"), ("Song", "ko")]
-users = [("Test", "en")]
 ExecuteCode = ExecuteCode(users)
 ExecuteCode.users_execute()
 
