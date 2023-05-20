c1adcc7a02f4e4901cf0b9f3cd77df34858fea08
AndyLeeProjects <anddy0622@gmail.com>
2023-05-20 18:07:26 -0400

    gitignore

--- src/db_connections.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine, text
-
-user = 'postgres'
-pw = 'postgres'
-server = '199.241.139.206:5432'
-db = 'vocab_storage'
-
-con = create_engine(f'postgresql://{user}:{pw}@{server}/{db}').connect()
